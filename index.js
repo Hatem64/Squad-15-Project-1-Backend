f7f7aa07ea66992b4030e560249686e8cf4661bb
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-10-07 13:35:40 +0200

    finish init the repo

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ import dotenv from "dotenv";
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import connectDB from "./db/connect.js";
-import userRouter from "./routes/user.js"
+// import userRouter from "./routes/user.js"
 import morgan from "morgan"
-import path from 'path'
+
 // CONSTANTS
 dotenv.config();
 const PORT = process.env.PORT || 5001;
-// const DBURI = process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017';
+const DBURI = process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017';
+
 const app = express();
-// to enable cookies
-app.enable('trust proxy');
 //CORS  OPTIONS
 
 const corsOptions ={
-    origin:/https:\/\/(.+).vercel.app/, 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
 }
 
@@ -27,11 +26,8 @@ app.use(cookieParser());
 app.use(morgan('tiny'))
 
 //ROUTES
-app.use('/api/users',userRouter);
-// app.use('/api/tasks',taskRouter)
-// app.all('*', function(req, res){
-//     res.status(404).send(process.env.FRONT_URL);
-//   });
+// app.use('/api/users',userRouter);
+
 
 // SERVER
 connectDB(DBURI);
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 
 router.post('/login',login)
+router.post('/signup',signup)
 
 router.get('/verify/:token',verifyEmail)
 
