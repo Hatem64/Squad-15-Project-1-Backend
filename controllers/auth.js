0797239a0090102482f2c073ead329adad3aa4fd
Nour Sherif <noorsherifm@gmail.com>
2022-10-25 15:34:37 +0200

    signup and login with authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,52 +1,94 @@
 import { response } from 'express';
 import User from '../model/User.js'
-import fail from "../utils/apiUtils.js"
+import {fail, success} from "../utils/apiUtils.js"
+import jwt from 'jsonwebtoken'
+import crypto from "crypto"
+import {promisify} from "util"
+
+const secret = process.env.JWT_SECRET || 'test';
+
+const signToken = (id)=>{
+    /**
+     * Create jwt token
+     */
+    // return jwt.sign({id},process.env.JWT_SECRET,{
+    //     expiresIn:process.env.JWT_EXPIRES_IN
+    // })
+    return jwt.sign({id},secret,{
+       // expiresIn:process.env.JWT_EXPIRES_IN
+       expiresIn:1000
+    })
+
+}
+
+const createSendToken = (user,statusCode,res,req)=>{
+    /**
+     * create token
+     * create cookie 
+     * send  response to user 
+     */
+    const token = signToken(user._id);
+    const cookieOptions = {
+        //expires:new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000),
+        expires:new Date(Date.now() + 1000*24*60*60*1000),
+        httpOnly:true,
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+        sameSite:"None",
+    };
+    res.cookie('jwt',token,cookieOptions);
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        ...success(user),
+        token
+    })
+}
 
 export const signup = async(req,res,next) => {
     try {
-        const {username,email,password} = req.body;
+        const {username,e_mail,password} = req.body;
+        const existingUser = await User.findOne({e_mail});
+        if(existingUser){
+            throw new Error('user already registered');
+        }
+       
         const newUser = await User.create({
             username,
-            email,
+            e_mail,
             password
         })
+    
       
         if(!newUser) throw new Error('Couldn\'t create user');
+     
         await newUser.save();
-           //return  CreateAndSendEmailVerification(newUser,req,res);
-           //res.send(newUser);
+           res.send("user successfully registered");
 
     } catch (error) {
-        req.send("error here");
-       //res.status(500).json(fail(error.message));
+        //res.send("error here");
+       res.status(500).json(fail(error.message));
     }
 }
 
 export const login = async(req, res, next) => {
     // GET EMAIL AND PASSWORD FROM REQUEST
-    const {email,password} = req.body;
+    const {e_mail,password} = req.body;
     //res.send(email);
     try {
         // CHECK EMAIL AND PASSWORD EXISTS
-        if(!email || !password) {
+        if(!e_mail || !password) {
             throw new Error("Please provide email and password")
         }
         // FIND USER BY EMAIL
-        const user = await User.findOne({email})
-        // .select('+password')
+        const user = await User.findOne({e_mail}).select('+password')
+
         if(!user) throw new Error("No such user")
-      // else{ res.send("user logged in" + email)} 
-        // CHECK IF ACCOUNT IS VERIFIED
-    //     // if(!user.verified){
-    //     //     return CreateAndSendEmailVerification(user,req,res);
-    //     // }
+          if(!user || ! (await user.correctPassword(password,user.password))) {
+      throw new Error("Invalid email or password")}
+    //res.send("user successfully logged in")
 
-    //     // if(!user || ! (await user.correctPassword(password,user.password))) {
-    //     //     throw new Error("Invalid email or password")
-
-    //     // }
-    //     // // CREATE AND SEND NEW JWT TOKEN
-    //     // createSendToken(user,200,res,req);
+    // CREATE AND SEND NEW JWT TOKEN
+     createSendToken(user,200,res,req);
 
     } catch (error) {
         res.status(400).json(fail(error.message));
--- model/User.js
+++ model/User.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
+import crypto from "crypto";
 //import Apprentice from "./Apprenticeship";
 const { Schema, SchemaTypes, model } = mongoose;
 const userSchema = new Schema({
@@ -6,6 +8,16 @@ const userSchema = new Schema({
   password: String,
   e_mail: String,
   apprentice: [{ type: SchemaTypes.ObjectId, ref: "Apprentice" }],
+});
+userSchema.methods.correctPassword = async function(candidatePassword,userPassword)
+{
+    return await bcrypt.compare(candidatePassword,userPassword)
+}
+userSchema.pre('save',async function (next){
+  if(!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password,12);
+  next();
 });
 
 const User = model("User", userSchema);
